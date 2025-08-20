2144824581f8dc6916450cf72834f237b0ecb387
bingtang888 <321635916@qq.com>
2025-08-20 23:41:05 +0800

    指针题目一:编写一个Go程序，定义一个函数，该函数接收一个整数指针作为参数，在函数内部将该指针指向的值增加10，然后在主函数中调用该函数并输出修改后的值。

---
+++ test1/pointer_example.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func addTen(num *int) {
+	*num += 10
+}
+
+func main() {
+	value := 20
+	fmt.Printf("修改前的值：%d\n", value)
+
+	addTen(&value)
+	fmt.Printf("修改后的值：%d\n", value)
+}
